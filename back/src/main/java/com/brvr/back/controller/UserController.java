8080f8bca062d1006a950d91723e5248634718ad
KBS <KBS@DESKTOP-183ILO7>
2023-09-15 00:59:09 +0900

    profile responseWraper

--- back/src/main/java/com/brvr/back/controller/UserController.java
+++ back/src/main/java/com/brvr/back/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import com.brvr.back.dao.UserDAO;
 import com.brvr.back.entity.User;
 import com.brvr.back.oauth2.GoogleOAuth2User;
 import com.brvr.back.repository.UserRepository;
+import com.brvr.back.utils.ResponseWraper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +30,7 @@ public class UserController {
 	
 	private final UserRepository userRepository;
 	private final UserDAO userDAO;
+	private final ResponseWraper responseWraper;
 	
 	/**
 	 * METHOD: GET
@@ -41,13 +44,15 @@ public class UserController {
     	Map<String, Object> map = new HashMap<>();
     	Optional<User> user = userRepository.findByEmail(email);
     	
+    	HttpStatus status = HttpStatus.BAD_REQUEST;
     	if(!user.isEmpty()) {
     		map.put("name", user.get().getName());
     		map.put("email", user.get().getEmail());
     		map.put("profile", user.get().getProfile());
+    		status =  HttpStatus.OK;
     	}
     	
-    	return map;
+    	return responseWraper.getProcessedResponse(map, status);
     }
     
 	/**
